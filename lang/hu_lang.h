d49ff658dd46127cc9d7fbdce98616541510e5c3
kayapo <kayapo@molnaristvan.eu>
2013-09-12 07:41:35 +0200

    A lang/hu_lang.h -t hozza adtam a kodhoz Ezzel lesz nyelvesitve a felulet

---
+++ lang/hu_lang.h
@@ -0,0 +1,158 @@
+#ifndef LANG_H
+#define LANG_H
+
+/*!
+ * @defined
+ * @abstract   DATE_SEPARATOR
+ * @discussion Date separator character
+ */
+#define DATE_SEPARATOR "."
+
+/*!
+ * @defined
+ * @abstract   DATE_SEPARATOR
+ * @discussion Date separator character
+ */
+#define CLOCK_SEPARATOR ":"
+
+/*!
+ * @defined
+ * @abstract   MSG_START
+ * @discussion Start message
+ */
+#define MSG_START "Inditas"
+
+/*!
+ * @defined
+ * @abstract   MSG_YES
+ * @discussion Yes
+ */
+#define MSG_YES "Igen"
+
+/*!
+ * @defined
+ * @abstract   MSG_NO
+ * @discussion No
+ */
+#define MSG_NO "Nem"
+
+/*!
+ * @defined
+ * @abstract   MSG_ON
+ * @discussion On
+ */
+#define MSG_ON "Be"
+
+/*!
+ * @defined
+ * @abstract   MSG_OFF
+ * @discussion Off
+ */
+#define MSG_OFF "Ki"
+
+/*!
+ * @defined
+ * @abstract   MSG_SETTINGS_MENU
+ * @discussion Settings menu message
+ */
+#define MSG_SETTINGS_MENU "Beallitas"
+
+/*!
+ * @defined
+ * @abstract   MSG_SET_CLOCK
+ * @discussion Clock setup message
+ */
+#define MSG_SET_CLOCK "Ora beallitas"
+
+/*!
+ * @defined
+ * @abstract   MSG_SET_DATE
+ * @discussion Date setup message
+ */
+#define MSG_SET_DATE "Datum beallitas"
+
+/*!
+ * @defined
+ * @abstract   MSG_MIN
+ * @discussion Minute word
+ */
+#define MSG_MIN "Perc"
+
+/*!
+ * @defined
+ * @abstract   MSG_HOUR
+ * @discussion Hour word
+ */
+#define MSG_HOUR "Ora"
+
+/*!
+ * @defined
+ * @abstract   MSG_DAY_OF_WEEK
+ * @discussion Day Of Week message
+ */
+#define MSG_DAY_OF_WEEK "Het napja"
+
+/*!
+ * @defined
+ * @abstract   MSG_DAY_OF_MONTH
+ * @discussion Day Of Month message
+ */
+#define MSG_DAY_OF_MONTH "Honap napja"
+
+/*!
+ * @defined
+ * @abstract   MSG_MONTH
+ * @discussion Month word
+ */
+#define MSG_MONTH "Honap"
+
+/*!
+ * @defined
+ * @abstract   MSG_YEAR
+ * @discussion Year setup message
+ */
+#define MSG_YEAR "Évszam"
+
+/*!
+ * @defined
+ * @abstract   MSG_NEW_PROGRAM
+ * @discussion New Program message
+ */
+#define MSG_NEW_PROGRAM "Uj program"
+
+/*!
+ * @defined
+ * @abstract   MSG_NEW_EVENT
+ * @discussion New Event message
+ */
+#define MSG_NEW_EVENT "Uj esemeny"
+
+/*!
+ * @defined
+ * @abstract   MSG_ZONE
+ * @discussion Zone word
+ */
+#define MSG_ZONE "Zona"
+
+/*!
+ * @defined
+ * @abstract   MSG_WATER_ON
+ * @discussion Water Open message
+ */
+#define MSG_WATER_ON "Vizet megnyit"
+
+/*!
+ * @defined
+ * @abstract   MSG_WATER_OFF
+ * @discussion Water close message
+ */
+#define MSG_WATER_OFF "Vizet elzar"
+
+/*!
+ * @defined
+ * @abstract   MSG_WATER_DETECTOR
+ * @discussion Water detector message
+ */
+#define MSG_WATER_DETECTOR "Nedvesseg erz."
+
+#endif
